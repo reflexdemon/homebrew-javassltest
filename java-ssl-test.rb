dbb7dfb481e35c0aacc5b049b58fc6b9b360da9f
vprasanna <vprasanna@bridge2solutions.com>
2018-04-20 06:39:42 -0400

    Deployed java-ssltest-cli@1.0.12

--- java-ssl-test.rb
+++ java-ssl-test.rb
@@ -1,15 +1,16 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class JavaSslTest < Formula
   desc "Simple Java based CLI Tool to test SSL connection and list the ciphers"
   homepage ""
-  url "https://github.com/reflexdemon/java-ssl-test/archive/1.0.10.tar.gz"
-  sha256 "a778d6c3acd5f10094154bcb193c471213988d8d630e45de9c24c9aaa61fd9eb"
-  # depends_on "cmake" => :build
+  url "https://github.com/reflexdemon/java-ssl-test/archive/1.0.12.tar.gz"
+  sha256 "dd1d10fcf945a6ca951b905d3a8eff2126da066c388830d22700ab3e8ee89c50"
 
   def install
-    bin.install  "javassltest.sh"
-    bin.install  Dir["./jdeploy-bundle/*"]
+    bin.install  Dir["jdeploy-bundle/*"]
+  end
+
+  test do
+    system "#{bin}/welcome.sh"
   end
 end
